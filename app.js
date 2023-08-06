3df5c846da723bcf3853b43651491a43f5dbb28f
rizwan <rizwangeniousfms@gmail.com>
2023-08-06 20:55:41 +0500

    project initialize

---
+++ app.js
@@ -0,0 +1,21 @@
+//alert("Hello World")
+
+//var a = 5;
+//var b = 10;
+//var c = a+b
+//alert(c)
+
+// var a = 5;
+// var b = 10;
+// var c = a+b
+// console.log(c);
+
+// var x = "haider";
+// var y = "ali";
+// var z = y+x
+// alert(z);
+
+var f = 10;
+var g = "20" ;
+var h = f+g;
+alert(h); 
